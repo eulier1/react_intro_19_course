5233203d5744f579aab08149ca728b2bd7e36640
Eulier González <eulier1@gmail.com>
2025-05-08 15:50:37 -0300

    add prettier, code-formatting

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 const Pizza = (props) => {
-    return React.createElement("div", {}, 
-        React.createElement("h1", {}, props.name),
-        React.createElement("div", {}, props.description)
-    )
-}
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, props.name),
+    React.createElement("div", {}, props.description)
+  );
+};
 //"Pepperoni Pizza"
 // "Mozzarella Cheese, Pepperoni"
 
 const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1",  {}, "Padre Gino's"),
-        React.createElement(Pizza, {
-            name: "The Pepperoni Pizza",
-            description: "Mozzarella Cheese, Pepperoni",
-          }),
-        React.createElement(Pizza, {
-            name: "The Hawaiian Pizza",
-            description: "Sliced Ham, Pineapple, Mozzarella Cheese",
-          }),
-        React.createElement(Pizza, {
-            name: "The Big Meat Pizza",
-            description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
-          }),
-    ])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+  ]);
+};
 
-const container = document.getElementById('root')
+const container = document.getElementById("root");
 
-const root = ReactDOM.createRoot(container)
+const root = ReactDOM.createRoot(container);
 
-root.render(React.createElement(App))
+root.render(React.createElement(App));
