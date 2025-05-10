27f713a4d6260687f6d1cc7518fe24a744c10e76
Eulier González <eulier1@gmail.com>
2025-05-10 17:20:55 -0300

    add strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
 import Order from "./Order";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="logo">Padre Gino's Pizza</h1>
-      <Order />
-    </div>
+    <StrictMode>
+      <div>
+        <h1 className="logo">Padre Gino's Pizza</h1>
+        <Order />
+      </div>
+    </StrictMode>
   );
 };
 
