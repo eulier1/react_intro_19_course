37b687c3ae62955ecd2ad422b6a3fded376a7fc9
Eulier González <eulier1@gmail.com>
2025-05-13 08:25:26 -0300

    add tanstack-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { createRoot } from "react-dom/client";
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createRouter({ routeTree });
 
-const App = () => {
-  const cartHook = useState([]);
+const queryClient = new QueryClient();
 
+const App = () => {
   return (
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 };
